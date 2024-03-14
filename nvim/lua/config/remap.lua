393fc8bfb3b29c3f9a9a2999b13fb5cd926b4526
Nick Morgan <nick.morgan@coinbase.com>
2024-03-14 12:25:27 -0700

    update

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,1 +1,5 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -12,7 +12,7 @@ vim.opt.relativenumber = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -26,3 +26,4 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
+vim.opt.colorcolumn = "80"

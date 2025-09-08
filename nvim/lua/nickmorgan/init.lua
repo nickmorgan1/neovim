1aa97e2db21fa72f9fe5ae29bda27c2c039e420b
Nick Morgan <mjn0830@gmail.com>
2025-09-07 20:18:58 -0700

    update installation

--- nvim/lua/nickmorgan/init.lua
+++ nvim/lua/nickmorgan/init.lua
@@ -5,3 +5,56 @@ require("nickmorgan.lazy_init")
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+local augroup = vim.api.nvim_create_augroup
+local NickMorganGroup = augroup('NickMorgan', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd("BufWritePre", {
+    group = NickMorganGroup,
+    pattern = "*",
+    callback = function()
+        -- Save the view (cursor position, folds, etc.)
+        local save = vim.fn.winsaveview()
+
+        -- Restrict substitution to modified lines only
+        vim.cmd([[
+            silent! keeppatterns %s/\s\+$//e
+            silent! keeppatterns g/^\%#\%V/ s/\s\+$//e
+        ]])
+
+        -- Restore the view
+        vim.fn.winrestview(save)
+    end,
+})
+
+
+autocmd('LspAttach', {
+    group = NickMorganGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
--- nvim/lua/nickmorgan/lazy/lsp.lua
+++ nvim/lua/nickmorgan/lazy/lsp.lua
@@ -1,3 +1,14 @@
+local root_files = {
+  '.luarc.json',
+  '.luarc.jsonc',
+  '.luacheckrc',
+  '.stylua.toml',
+  'stylua.toml',
+  'selene.toml',
+  'selene.yml',
+  '.git',
+}
+
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -127,28 +138,6 @@ return {
         { name = "buffer" },
       }),
     })
-
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-
-          local builtin = require "telescope.builtin"
-
-          vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-          vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
-          vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-
-          vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
-          vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
-          vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
-          vim.keymap.set("n", "<space>ww", function()
-            builtin.diagnostics { root_dir = true }
-          end, { buffer = 0 })
-        end,
-      })
 
     vim.diagnostic.config({
       -- update_in_insert = true,
